d3c7c29d1c6f6ff96167d61265fbf195b5138280
tamilCodingWizard <tamilcodingwizard@gmail.com>
2022-11-27 08:07:06 +0000

    Video 04

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,32 @@
-import Image from 'next/image'
+import Image from "next/image";
+import client from "./../config/contentful";
+import { Grid } from '@mui/material';
+import TripCard from "../components/TripCard";
 
-export default function Home() {
-  return (
-    <div >
-      
-    </div>
-  )
+export const getStaticProps = async () => {
+  const response = await client.getEntries({
+    content_type: "trips",
+  });
+
+  return {
+    props: {
+      trips: response.items,
+    },
+  };
+};
+
+export default function Home({ trips }) {
+  console.log(trips);
+  return <Grid container spacing={2}>
+    {
+      trips && trips.map((trip) => {
+        return (
+          <Grid item key={trip.sys.id}>
+             <TripCard/>
+          </Grid>
+        )
+      })
+    }
+
+  </Grid>
 }
