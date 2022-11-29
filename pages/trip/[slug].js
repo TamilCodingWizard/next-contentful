437c134db49014c87d5130450fde2249984a035f
tamilCodingWizard <tamilcodingwizard@gmail.com>
2022-11-29 16:59:23 +0000

    video06

--- pages/trip/[slug].js
+++ pages/trip/[slug].js
@@ -1,6 +1,48 @@
 import React from 'react'
+import client from './../../config/contentful';
 
-const TripDetail = () => {
+
+export const getStaticPaths = async () => {
+  const response = await client.getEntries({
+    content_type:'trips'
+  })
+
+  // {params: {slug:val}}
+
+  const paths = response.items.map((trip) => {
+      return {
+        params: {
+          slug:trip.fields.slug
+        }
+      }
+  })
+
+  console.log('Paths')
+  console.log(paths)
+  return {
+    paths,
+    fallback:false
+  }
+}
+
+export const getStaticProps = async ({params}) => {
+
+    console.log('Params')
+    console.log(params)
+    const response = await client.getEntries({
+      content_type:'trips',
+      'fields.slug':params.slug
+    })
+
+    return {
+      props: {
+        trip:response.items[0]
+      }
+    }
+}
+
+const TripDetail = ({trip}) => {
+  console.log(trip)
   return (
     <div>TripDetail</div>
   )
