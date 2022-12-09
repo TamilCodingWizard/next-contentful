d01f3917c8c2846fe8e464a0f5de839a4d24b401
tamilCodingWizard <tamilcodingwizard@gmail.com>
2022-12-09 20:50:22 +0000

    pwa

--- next.config.js
+++ next.config.js
@@ -1,10 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register:true,
+  disable: process.env.NODE_ENV === 'development'
+})
+const nextConfig = withPWA({
   reactStrictMode: true,
   swcMinify: true,
   images: {
     domains:["images.ctfassets.net"]
   }
-}
+})
 
 module.exports = nextConfig
